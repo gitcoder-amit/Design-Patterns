3f5f48a4397e3d75a7225193b2666ee4f7ff0322
amit7815 <yadavamit14801@gmail.com>
2024-06-17 23:32:05 +0530

    reading file

--- daily_python/file_handling/mode_of_opening_file.py
+++ daily_python/file_handling/mode_of_opening_file.py
@@ -6,7 +6,33 @@ Mode of opening file :-
 
     Text Mode -> when we open a file  in text modes, python treats it's  content as str type
                 when we get data  from a text mode file, python first decodes the raw bytes using either a platform dependent encoding or specified encoding
+    
+    r --> Open file for reading only. If  the file doesn't exist. It will show FileNotFoundError
+    w --> open file for writing only. If the file doesn't exist. it will create a file
+    a --> open for appending. It appends new data at end of file. If file does not exist. It creates a new file
+    x --> open for exclusive creation for writing. The specified file must not be available. It creates a new file  and then  we write data into it. If file exists. It will give an error.
+    r+ --> Open for reading  and then writing 
+    w+ --> Open for writing and then reading
+    a+ --> Open for appending  and then writing
+
 
     Binary Mode -> when we open a file in binary mode, python uses the data without any encoding. Binary mode file    reflects the raw data directly in the file
         Python treats file content  as bytes type. These modes are used  while dealing  with nontext file like images, audios, videos etc
+
+    rb --> Open for reading in binary mode(same as text b)
+    wb -->Open for writing in binary mode( same as text w)
+    ab --> Open for appending (same as text a)
+    xb --> Open for exclusive creation and writing same as x
+    rb+ --> Open for read and then write in binary
+    wb+ --> Open for writing  and then reading in binary
+    ab+ --> Open for  append  and then read  in binary
 '''
+
+
+f = open("data.txt", mode = 'r')
+print(f.read())
+f.close()
+
+f = open('data.txt', mode = 'rb')
+print(f.read())
+f.close()
---
+++ daily_python/file_handling/reading_data_from_file.py
@@ -0,0 +1,40 @@
+'''
+    Reading data from file --> To read content of file, we have 3 methods
+        read()
+        readline()
+        readlines()
+
+        read() -> this method is used to read data/content from a file and returns it as a string in text mode. It returns bytes in binary mode --> file_object.read(size) --> size is optional
+
+        size -> it represents  the number of characters  to be read  in text mode. No need to give size in Binary mode
+
+        readline() --> this function is used to read a single line from a file
+        syntax -> file_object.readline(size)
+        size -> number of characters to read from line
+
+        readlines() -> this method is used to read all lines from a file and returns  a list of lines
+'''
+
+f = open('data.txt', mode = 'r')
+data = f.read(6)  # read 6 character from start
+print(data)
+data1 = f.read(2)  # next 2 character
+print(data1)
+f.close()
+
+
+f = open("data.txt", mode='r')
+data = f.readline() # to read first line 
+# data = f.readline(3) # to read first 3 character of line if we will check data 1 after then that will print whole first line except these 3 characters due to cursor is in first line after 3 character
+data1 = f.readline()  # second line due to pointer is now at second line
+print(data, end='')  # here is one \n for print and one \n for readline so we will get space after one line to remove this we need to use end = ' '
+print(data1)
+f.close()
+
+
+f = open('data.txt', mode='r')
+data = f.readlines()
+print(data)
+for line in data:
+    print(line, end="")
+f.close()
