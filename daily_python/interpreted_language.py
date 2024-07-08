4e4e16d4f05cf2023b1330f3c758f698e061adc4
amit7815 <yadavamit14801@gmail.com>
2024-07-08 13:50:00 +0530

    interpreter

--- daily_python/interpreted_language.py
+++ daily_python/interpreted_language.py
@@ -20,4 +20,6 @@ python source code -> compiler --> binary code
 now we can distribute this binary code as a software to windows, linux, mac but this code is not understandable so not able to run this code due to no operating system can understand this byte code. If we want to make this code understand by these Operating system, for that it is necesarry to install one more software in these OS that is known as Python Virtual Machine, This PVM contains interpretere which we said interpreter of python so technically this is not an interpreter just it is a just intime compiler. There are multiple things in PVM that requires to run this byte code. This PVM is different for all OS, PVM translates this Byte code into machine code so that OS can understand run this code.
 
 Just in time compiler basically reads line one by one in byte code and translate into machine code and provides one by one to OS so that OS can execute when OS is executing one line at that time just in time compiler reads another line convert into machine code and store these line to provide when OS free from previous line and provide to OS and so on
+
+Just in compiler is improved version of Interpreter because Interpreter after converting one line into machine code waits for OS to process another line.
 '''
